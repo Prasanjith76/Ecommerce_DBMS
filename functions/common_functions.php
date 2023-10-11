8b64e5f58b969e7a9342426b946f9f369bccb0e4
Prasanjith76 <prasanjith123.lorensuhewa@gmail.com>
2023-10-11 21:22:10 +0530

    Updated a sql query using join

--- functions/common_functions.php
+++ functions/common_functions.php
@@ -156,7 +156,8 @@ function  view_product_details()
             <option selected class='d-none'>Select Variant</option>";
     }
     $iteration = 0;
-    $select_varients = "Select * from product_variant, variant where product_variant.Variant_ID = variant.Variant_ID AND Product_ID=$product_id";
+    // "Select * from product_variant, variant where product_variant.Variant_ID = variant.Variant_ID AND Product_ID=$product_id";
+    $select_varients = "Select * from product_variant JOIN variant USING(Variant_ID) where Product_ID=$product_id";
     $result_varients = mysqli_query($adminconnection, $select_varients);
     $option_val = 0;
     while ($row_data = mysqli_fetch_assoc($result_varients)) {
