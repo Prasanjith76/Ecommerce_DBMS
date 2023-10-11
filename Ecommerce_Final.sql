a79359dca329633a0eef8d0a4e51a8a1b69eaad1
Prasanjith76 <prasanjith123.lorensuhewa@gmail.com>
2023-10-11 21:21:02 +0530

    Added localhost database connection

--- Ecommerce_Final.sql
+++ Ecommerce_Final.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
--- Host: ecommerce-site-db.ck2iogphtt9t.ap-northeast-1.rds.amazonaws.com    Database: Ecommerce-db
+-- Host: localhost    Database: ecommerce-db
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
